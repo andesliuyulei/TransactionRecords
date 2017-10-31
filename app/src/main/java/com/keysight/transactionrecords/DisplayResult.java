4b53a1992d0872b1150515b2e612a58b11e93626
andesliuyulei <andesliuyulei@gmail.com>
2017-10-31 13:57:57 +0800

    2017-10-31 initial release.

---
+++ app/src/main/java/com/keysight/transactionrecords/DisplayResult.java
@@ -0,0 +1,24 @@
+package com.keysight.transactionrecords;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_result);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+
+        ((TextView) findViewById(R.id.textView)).setText("New transaction has been added successfully." +
+                "\nAccount: " + intent.getStringExtra(MainActivity.transactionAccount) +
+                "\nDate: " + intent.getStringExtra(MainActivity.transactionDate) +
+                "\nAmount: " + intent.getStringExtra(MainActivity.transactionAmount) +
+                "\nRemark: " +  intent.getStringExtra(MainActivity.transactionRemark));
+    }
+}
